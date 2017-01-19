562c1e8e26eb8a2ce7d12e5add2e02d85c955875
warrenbrown <warren@katapultmedia.com>
2017-01-19 10:57:53 -0600

    Designed Landing page
    Finished designing the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   get 'angular_test', to: 'angular_test#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
